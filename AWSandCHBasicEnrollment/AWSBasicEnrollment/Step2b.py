611fb65ffa669e8c1256b2de2d87aa4a05b5d85b
jmalach <jmalach@LT-A0062477.corp.cdw.com>
2018-11-13 15:44:35 -0600

    Update comments in Step2b

--- AWSandCHBasicEnrollment/AWSBasicEnrollment/Step2b.py
+++ AWSandCHBasicEnrollment/AWSBasicEnrollment/Step2b.py
@@ -7,10 +7,9 @@ import boto3
 from botocore.exceptions import ClientError
 
 try:
-    # this code writes a new profile to the AWS config file to allow BlueMoon to use the CDWCustomerAccountAccessRole
-    # to administer customer resources in the target environment
-
-    # this next section creates a stack instance from the BlueMoon account to run in the target account
+    # this code copies the new config.json file to the CFT bucket for use by the state machine
+    # the object is made public read to allow for all linked accounts to read the file
+    
     client = boto3.client('s3')
     
     response = client.put_object(
