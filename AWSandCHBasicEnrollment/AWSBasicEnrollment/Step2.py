42872aa59c8fc8e1b8af51d03249a5a876e1895e
jmalach <jmalach@LT-A0062477.corp.cdw.com>
2018-11-13 15:24:03 -0600

    Added Comments to Step2 and Step3

--- AWSandCHBasicEnrollment/AWSBasicEnrollment/Step2.py
+++ AWSandCHBasicEnrollment/AWSBasicEnrollment/Step2.py
@@ -13,14 +13,20 @@ account = parameters[1]
 profile = parameters[2]
 
 try: 
+    # by using the profile included in the arguments, we can switch to the master account 
+    # this allows use of the OrganizationAccountAccessRole to create the other two cross account access roles
     session = boto3.Session(profile_name=profile)
     sts_client = session.client('sts')
 
     role = "arn:aws:iam::" + account + ":role/OrganizationAccountAccessRole"
     sessionId = "create-cross-account-roles" 
+
+    # next access the STS service to receive credentials for the OrganizationAccountAccessRole
+    # once we have the credentials, we will use them to create the next boto3 client
     sts_response = sts_client.assume_role(RoleArn=role, RoleSessionName=sessionId)
     credentials = sts_response['Credentials']
     
+    # the new roles will allow BlueMoon enabled engineers to use the cross account access roles
     my_managed_policy = {
       "Version": "2012-10-17",
       "Statement": [
@@ -34,11 +40,13 @@ try:
         }
       ]
     }
-    
+    # unlike the typical client invocations you need to supply the credentials and the session token received from 
+    # the earlier call to STS
     client=boto3.client('iam',aws_access_key_id=credentials['AccessKeyId'],
         aws_secret_access_key=credentials['SecretAccessKey'],
         aws_session_token=credentials['SessionToken'])
     
+    # create the first role and apply the permissions policy
     response = client.create_role(
         Path="/",
         RoleName='CDWCustomerAccountAccessRole-joe',
@@ -63,7 +71,7 @@ try:
         RoleName='CDWCustomerAccountAccessRole-joe'
     )
     
-    #REM the next two commands create the AWSCloudFormationStackSetExecutionRole
+    # create the second role and apply the permissions policy
     response = client.create_role(
         Path="/",
         RoleName='AWSCloudFormationStackSetExecutionRole-joe',
--- AWSandCHBasicEnrollment/AWSBasicEnrollment/Step3.py
+++ AWSandCHBasicEnrollment/AWSBasicEnrollment/Step3.py
@@ -18,6 +18,9 @@ arn = "arn:aws:states:us-east-2:"+account+":stateMachine:CloudHealth-Basic-Enrol
 input_line="{\"role\":\"CDW-CH-ReadOnly-Role\",\"account\":\""+account+"\",\"newaccount\":"+newaccount+"}"
 
 try:
+    # in order to start the state machine execution the user needs to user the profile in the AWS config
+    # file to use the CDWClientAccountAccessRole
+    
     session = boto3.Session(profile_name=profile)
     client = session.client('stepfunctions')
     
